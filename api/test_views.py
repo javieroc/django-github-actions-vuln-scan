d48843bbb4f8f0254cd6447b8c6347b18b04e5f1
Javier Ocampo <ferroxido@gmail.com>
2025-09-12 20:01:34 +0100

    github action: run vuln scanner

--- api/test_views.py
+++ api/test_views.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 class HelloWorldTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='testpassword', id=1)
+
     def test_hello_world(self):
         client = APIClient()
-        response = client.get('/api/hello/')
+        response = client.get('/api/hello/?id=1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {"message": "Hello, World!"})
+        self.assertIn('user', response.data)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db import connection
 
 class HelloWorldView(APIView):
     def get(self, request):
-        return Response({"message": "Hello, World!"})
+        cursor = connection.cursor()
+        user_id = request.query_params.get("id", '1')
+        cursor.execute("SELECT * FROM auth_user WHERE id = " + user_id)
+        user = cursor.fetchone()
+        return Response({"user": user})
